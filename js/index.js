7ecf4e9046f0533287fbbb43c6700561e43b3293
unknown <1913471527@qq.com>
2018-09-13 16:30:13 +0800

    aaa

--- js/index.js
+++ js/index.js
@@ -6,9 +6,11 @@ $(function(){
 
     $(".head-right").click(function () {
         $(".head-bottom").toggle();
-        $(".head-bottom").flush();
+        // $(".head-bottom").load(location.href+" .head-bottom");
+        // $(".head-bottom").flush();
         // $(".head-bottom").css("display","block");
         // $(".head-bottom").css("display","none");
+        // window.location.reload()
     })
 
 
